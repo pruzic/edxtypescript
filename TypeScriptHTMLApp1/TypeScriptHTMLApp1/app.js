3e768eb060d742ca288052d1b6d6b5c1bcbed624
predrag.ruzic <predrag.ruzic@interpublic.com>
2016-11-30 13:04:14 -0600

    VS 2015 TypeScript

---
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/app.js
@@ -0,0 +1,46 @@
+﻿var name = "Michael Crump";
+var fullName = "Michael Crump";
+
+var num = 1;
+var bigNumber = 123;
+
+var something;
+
+function helloPerson(s1) {
+    return s1;
+}
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+addNumbers(234, "Predrag");
+
+helloPerson(name);
+
+//var b:IBook = {
+//    title: "Moby Dick",
+//    author: "Herman Melville",
+//    bookInfo: function () {
+//        return this.title + " by : " + this.author;
+//    }
+//}
+var MyBook = (function () {
+    function MyBook() {
+        this.title = "Moby Dick";
+        this.author = "Predrag Ruzic";
+    }
+    MyBook.prototype.bookInfo = function () {
+        return this.title + " by: " + this.author;
+    };
+    return MyBook;
+})();
+
+var b = new MyBook();
+
+b.title = "Serbian Nature";
+b.author = "Predrag Ruzic";
+
+var book = b.bookInfo();
+alert(book);
+//# sourceMappingURL=app.js.map
---
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/app.ts
@@ -0,0 +1,52 @@
+﻿
+var name = "Michael Crump";
+var fullName = "Michael Crump";
+
+var num = 1;
+var bigNumber = 123;
+
+var something;
+
+function helloPerson(s1: string) {
+    return s1;
+}
+
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+addNumbers(234, "Predrag");
+
+helloPerson(name);
+
+
+interface IBook {
+    title: string;
+    author: string;
+    bookInfo: () => string;
+}
+
+//var b:IBook = {
+//    title: "Moby Dick",
+//    author: "Herman Melville",
+//    bookInfo: function () {
+//        return this.title + " by : " + this.author;
+//    }
+//}
+
+class MyBook implements IBook {
+    title = "Moby Dick";
+    author = "Predrag Ruzic";
+    bookInfo() {
+        return this.title + " by: " + this.author;
+    }
+}
+
+var b = new MyBook();
+
+b.title = "Serbian Nature";
+b.author = "Predrag Ruzic";
+
+var book = b.bookInfo();
+alert(book);
